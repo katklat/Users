3215f0731154264be430a80cd14da50b4264d5b7
katklat <kinz@protonmail.com>
2020-07-13 10:26:52 +0200

    Add registration model

---
+++ database/migrations/2020_07_12_080543_create_registrations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamp('registered_at')->useCurrent();
+            $table->timestamp('unregistered_at')->nullable();
+            $table->time('time_registered')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrations');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ Route::prefix('/')->middleware('auth')->group(function () {
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-
 Route::get('/home', 'HomeController@index')->name('home');
 
 });
